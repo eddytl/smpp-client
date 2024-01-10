ee60d9f0a1cdd49c0993a48faa6a9cf1ab1fb1f0
eddytl <eddy.tchousse@nexah.net>
2024-01-10 12:28:57 +0100

    _

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 sudo mvn clean install
-sudo scp -r target/smpp-client-0.0.2-SNAPSHOT.jar eddy@ec2-3-143-150-184.us-east-2.compute.amazonaws.com:/var/www/html/smpp
-sudo ssh eddy@ec2-3-143-150-184.us-east-2.compute.amazonaws.com
+sudo scp -r target/smpp-mtn-0.0.1-SNAPSHOT.jar eddy@ec2-3-134-44-92.us-east-2.compute.amazonaws.com:/var/www/html/smpp
+sudo ssh eddy@ec2-3-134-44-92.us-east-2.compute.amazonaws.com
